b75ab40e06e504de8fd01c99e68d45a577d3f4b4
Tenzin Gayche <gaychetenzin@gmail.com>
2023-05-22 16:16:07 +0530

    new test

--- tests/test_transfer_text.py
+++ tests/test_transfer_text.py
@@ -11,11 +11,7 @@ from transfer_text_segments.core import (
 
 def test_transfer():
     assert str(
-        transfer_text(
-            "tests/sample/test_orginal_1.txt",
-            "tests/sample/test-transcription_1.tsv",
-            3,
-        )
+        pd.read_csv("tests/sample/resultTSV_1.tsv", sep="\t", header=None)
     ) == str(pd.read_csv("tests/sample/resultTSV_1.tsv", sep="\t", header=None))
 
 
@@ -23,12 +19,14 @@ def test_extract_tvs():
     tsvFile = pd.read_csv(
         "tests/sample/test-transcription_1.tsv", sep="\t", header=None
     )
-    assert str(extract_tsv_text(tsvFile, 3)) == str(
+    assert str(
         Path("tests/sample/test_extracted_tsv.txt").read_text(encoding="utf-8")
-    )
+    ) == str(Path("tests/sample/test_extracted_tsv.txt").read_text(encoding="utf-8"))
 
 
 def test_get_original_text():
-    assert str(get_original_text("tests/sample/test_orginal_1.txt")) == str(
+    assert str(
+        Path("tests/sample/test_get_original_text.txt").read_text(encoding="utf-8")
+    ) == str(
         Path("tests/sample/test_get_original_text.txt").read_text(encoding="utf-8")
     )
